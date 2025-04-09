88663a56e6a0282180dd81f84e7a2405033e2424
Everton Xavier <evertonfxavier@gmail.com>
2025-04-09 14:05:31 -0300

    initial commit

---
+++ tsdx.config.js
@@ -0,0 +1,31 @@
+// tsdx.config.js
+const postcss = require('rollup-plugin-postcss');
+const replace = require('@rollup/plugin-replace');
+
+module.exports = {
+  rollup(config, options) {
+    config.plugins = config.plugins.filter(plugin => plugin.name !== 'replace');
+
+    config.plugins.push(
+      replace({
+        preventAssignment: true,
+        'process.env.NODE_ENV': JSON.stringify(options.env || 'development'),
+      })
+    );
+
+    config.plugins.push(
+      postcss({
+        config: {
+          path: './postcss.config.js',
+        },
+        extensions: ['.css'],
+        minimize: true,
+        inject: {
+          insertAt: 'top',
+        },
+      })
+    );
+
+    return config;
+  },
+};
